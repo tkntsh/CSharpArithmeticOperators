aeb4bfc21734ffc065f78215fdde6856283888dc
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:15:41 +0200

    Create arithmetic7.cs
    Using three variables, calculate the number of months on earth for a 25 year old.
    Call your calculation variable to the Console.
    What is the answer?

---
+++ arithmetic7.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            int monthsInAYear = 12;
+            int ageInMonths = age * monthsInAYear;
+            Console.WriteLine(ageInMonths);
+        }
+    }
+}
