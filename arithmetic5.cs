1c96eadc40aba144b82630732bf5b0ed3130d23f
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:14:34 +0200

    Create arithmetic5.cs
    Print the remainder of 15 from 2 multiplied by 6 to the Console.
    What is the answer?

---
+++ arithmetic5.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int remainder = 15 % (2 * 6);
+            Console.WriteLine(remainder);
+        }
+    }
+}
