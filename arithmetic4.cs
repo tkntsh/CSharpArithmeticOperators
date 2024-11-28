d72f06bf1ad565ec7a827e31cac996a0544f50ce
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:14:00 +0200

    Create arithmetic4.cs
    Use the numbers 19, 12, 2 to print the answer 25 to the Console.

---
+++ arithmetic4.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 19 + (12 / 2);
+            Console.WriteLine(sum);
+        }
+    }
+}
