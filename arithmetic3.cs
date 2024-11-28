f3f1b7c6cfd2853f4790081184730c467918e909
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:13:28 +0200

    Create arithmetic3.cs
    Print the remainder of 11 from 3 to the Console.
    What is the answer?

---
+++ arithmetic3.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int remainder = 11%3;
+            Console.WriteLine(remainder);
+        }
+    }
+}
