aea1dba1928f82fa99d154cbe3f448438738ee95
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:12:52 +0200

    Create arithmetic2.cs
    Print the sum of 3 add 9 multiplied by 6 to the Console.
    What is the answer?

---
+++ arithmetic2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 3 + (9*6);
+            Console.WriteLine(sum);
+        }
+    }
+}
