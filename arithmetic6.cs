2e4ec06e18b08763bb17a7a14c6db6c9cfc76ffb
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:15:01 +0200

    Create arithmetic6.cs
    Create two variables named age and age next year.
    Assign 20 to age.
    Assign age add 1 to age next year.
    Print age next year to the Console.
    What is the answer?

---
+++ arithmetic6.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            int ageNextYear = age + 1;
+            Console.WriteLine(ageNextYear);
+        }
+    }
+}
